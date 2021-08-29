5ae349934d28aaf65060475a22bc936eb22a3c8b
Ekwugha <elloekwugha@gmail.com>
2021-08-29 01:21:25 +0100

    commit

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { link } from 'react-router-dom';
+
+import './header.style.scss';
+
+const Header = () => (
+    <div className='header'>
+        <link to="/">
+             
+        </link>
+    </div>
+)
